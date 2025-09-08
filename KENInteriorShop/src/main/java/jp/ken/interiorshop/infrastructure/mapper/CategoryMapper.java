d2ca94a99c692260b4c5cd2386a09ff11379393d
ken <ken@umeda-zosho-015.local>
2025-09-08 18:22:16 +0900

    commit

--- KENInteriorShop/src/main/java/jp/ken/interiorshop/infrastructure/mapper/CategoryMapper.java
+++ KENInteriorShop/src/main/java/jp/ken/interiorshop/infrastructure/mapper/CategoryMapper.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.RowMapper;
 import jp.ken.interiorshop.domain.entity.CategoryEntity;
 
 public class CategoryMapper implements RowMapper<CategoryEntity> {
+	//DBから取得したカテゴリテーブルの値をカテゴリEntityにセット
 	
 	@Override
 	public CategoryEntity mapRow(ResultSet rs, int rowNum) throws SQLException{
