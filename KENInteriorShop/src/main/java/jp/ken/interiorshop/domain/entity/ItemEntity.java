fdd1692fb84dfb12441cc8c0db9e0c715f6255f0
ken <ken@umeda-zosho-014.local>
2025-09-08 12:28:23 +0900

    Form/Entity

--- KENInteriorShop/src/main/java/jp/ken/interiorshop/domain/entity/ItemEntity.java
+++ KENInteriorShop/src/main/java/jp/ken/interiorshop/domain/entity/ItemEntity.java
@@ -1,6 +1,25 @@
 package jp.ken.interiorshop.domain.entity;
 
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
 public class ItemEntity {
 
-	private String item;
+	private int itemid;
+	
+	private String itemname;
+	
+	private int categoryid;
+	
+	private int itemprice;
+	
+	private Date rsdate;
+	
+	private String image;
+	
+	private String explanation;
+	
+	private int stock;
 }
