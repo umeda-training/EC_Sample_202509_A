1ee0e64e4f2d89427812bbdbc715957fc8e271f3
ken <ken@umeda-zosho-015.local>
2025-09-08 18:20:42 +0900

    commit

--- KENInteriorShop/src/main/java/jp/ken/interiorshop/infrastructure/mapper/CategoryMapper.java
+++ KENInteriorShop/src/main/java/jp/ken/interiorshop/infrastructure/mapper/CategoryMapper.java
@@ -1,5 +1,22 @@
 package jp.ken.interiorshop.infrastructure.mapper;
 
-public class CategoryMapper  {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
+import jp.ken.interiorshop.domain.entity.CategoryEntity;
+
+public class CategoryMapper implements RowMapper<CategoryEntity> {
+	
+	@Override
+	public CategoryEntity mapRow(ResultSet rs, int rowNum) throws SQLException{
+		
+		CategoryEntity categoryEntity = new CategoryEntity();
+		
+		categoryEntity.setCategoryId(rs.getInt("category_id"));
+		categoryEntity.setCategoryName(rs.getString("category_name"));
+		
+		return categoryEntity;
+	}
 }
