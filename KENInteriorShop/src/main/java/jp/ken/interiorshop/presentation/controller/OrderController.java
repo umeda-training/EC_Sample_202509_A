9bd95aded94af556c8e2c239abd63b73bd2409d2
ken <ken@umeda-zosho-014.local>
2025-09-09 16:15:32 +0900

    ControllerAdvice更新

--- KENInteriorShop/src/main/java/jp/ken/interiorshop/presentation/controller/OrderController.java
+++ KENInteriorShop/src/main/java/jp/ken/interiorshop/presentation/controller/OrderController.java
@@ -1,5 +1,25 @@
 package jp.ken.interiorshop.presentation.controller;
 
+import java.util.List;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+import jp.ken.interiorshop.presentation.form.ItemForm;
+
 public class OrderController {
 
+	
+	
+	@GetMapping("/order/confirm")
+	public String confirmOrder(HttpSession session, Model model) {
+	    Integer total = (Integer) session.getAttribute("cartTotal");
+	    List<ItemForm> itemNames = (List<ItemForm>) session.getAttribute("session_cart");
+
+	    model.addAttribute("cartTotal", total);
+	    model.addAttribute("cartItemNames", itemNames);
+
+	    return "orderConfirm"; // orderConfirm.html
+	}
 }
