8bc08fba34540f27a018b2bb0a5fc6e819fc720d
ken <ken@umeda-zosho-012.local>
2025-09-09 14:00:50 +0900

    0909_午前中まで

--- KENInteriorShop/src/main/java/jp/ken/interiorshop/presentation/controller/MemberController.java
+++ KENInteriorShop/src/main/java/jp/ken/interiorshop/presentation/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jp.ken.interiorshop.application.service.LoginService;
 import jp.ken.interiorshop.presentation.form.MemberLoginForm;
@@ -22,7 +23,6 @@ public class MemberController {
 		this.loginService = loginService;
 	}
 	
-	
 	//ログイン画面表示
 	@GetMapping("/login")
 	public String memberLoginForm(@ModelAttribute MemberLoginForm form) {
@@ -30,24 +30,32 @@ public class MemberController {
 	}
 	
 	//ログイン処理
-	@PostMapping("/login")
+	@PostMapping(value = "/login", params = "login")
 	 public String doLogin(@Valid @ModelAttribute MemberLoginForm form,
-			 BindingResult result,Model model) {
+			 BindingResult result,Model model, HttpServletRequest request) {
 		
+		//直前のURLを取得
+		String url = request.getRequestURI();
+		
+		//エラー時にログイン画面に戻る
 		if(result.hasErrors()) {
 			return "memberLogin";
 		}
 		
+		
 		try {
+			//入力されたメールアドレスとパスワードを取得
 			MemberLoginForm searchForm = new MemberLoginForm();
 			searchForm.setMail(form.getMail());
 			searchForm.setPassword(form.getPassword());
 			
-			List<MemberLoginForm> list = loginService.getMemberList(searchForm);
+			//全メンバー情報を取得し、リストに保存
+			List<MemberLoginForm> login = loginService.getMemberList(searchForm);
 			
 			boolean match = false;
 			
-			for(MemberLoginForm mem : list) {
+			//メールアドレスとパスワードが一致していれば、matchをtrueに
+			for(MemberLoginForm mem : login) {
 				if(mem.getMail().equals(form.getMail())
 				&& mem.getPassword().equals(form.getPassword())){
 					match = true;
@@ -57,7 +65,9 @@ public class MemberController {
 			}
 			
 			if(match) {
-				return "item"; //本来は検索画面に遷移
+				//メールアドレスとパスワードが一致していれば、ログイン情報をmodelに保存
+				model.addAttribute(login);
+				return url; 
 			}else {
 				model.addAttribute("loginError", "従業員IDまたは氏名が正しくありません");
 				return "memberLogin";
@@ -69,6 +79,16 @@ public class MemberController {
 			return "memberLogin";
 		}
 					
+	}
+	
+	//ログアウト処理
+	@PostMapping(value = "/logout")
+	public String doLogout(@ModelAttribute List<MemberLoginForm> login, HttpServletRequest request) {
+		
+		String url = request.getRequestURI();
+		login = null;
+		
+		return url;
 	}
 	
 }
