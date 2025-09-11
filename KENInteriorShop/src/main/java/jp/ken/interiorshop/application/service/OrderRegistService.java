c1cf25ca6829d43078b0e8e800437acfd4686d1e
ken <ken@umeda-zosho-014.local>
2025-09-11 12:43:31 +0900

    更新

--- KENInteriorShop/src/main/java/jp/ken/interiorshop/application/service/OrderRegistService.java
+++ KENInteriorShop/src/main/java/jp/ken/interiorshop/application/service/OrderRegistService.java
@@ -36,7 +36,7 @@ public class OrderRegistService {
 		//発送情報登録して発送IDを取得
 		int shippingId = orderRegistRepository.shippingRegist(shippingEntity);
 		//注文内容登録
-		int orderId = orderRegistRepository.orderRegist(shippingId, memberEntity.getMemberId(), orderEntity);
+		List<OrderDetailsForm> orderId = orderRegistRepository.orderRegist(shippingId, memberEntity.getMemberId(), orderEntity);
 		//注文詳細内容登録
 		orderRegistRepository.orderDetailsRegist(orderId, orderForm.getOrderDetailsForm().getItemId(), orderForm.getOrderDetailsForm().getItemQuantity(), orderForm.getOrderDetailsForm().getSubtotal());
 		
