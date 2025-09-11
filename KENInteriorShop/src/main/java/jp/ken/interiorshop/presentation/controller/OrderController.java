c88c4ffcd7389dd949ae5fbb962ca3eb63794ab1
ken <ken@umeda-zosho-014.local>
2025-09-11 14:58:31 +0900

    controller更新

--- KENInteriorShop/src/main/java/jp/ken/interiorshop/presentation/controller/OrderController.java
+++ KENInteriorShop/src/main/java/jp/ken/interiorshop/presentation/controller/OrderController.java
@@ -72,11 +72,17 @@ public class OrderController {
 	
 	@PostMapping(value="/ordercomp")
 	public String completeOrder(@ModelAttribute OrderForm orderForm,
-	        @SessionAttribute("loginUser") MemberLoginForm form)  {
+	        @SessionAttribute("loginUser") MemberLoginForm form,
+	        HttpSession session)  {
 		
+		// サービスで注文情報を登録（発送、注文、注文詳細の3テーブル）
 		orderRegistService.orderRegist(form, orderForm);
 		
-		
+		// カートのセッションを破棄
+	    session.removeAttribute("cart");
+	    session.removeAttribute("totalExclTax");
+	    session.removeAttribute("totalTax");
+	    session.removeAttribute("totalInclTax");
 
 		 return "orderComplete";
 	}
