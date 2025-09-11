96b37a0c868534be66e8295180a8b498e33896a8
ken <ken@umeda-zosho-012.local>
2025-09-11 11:40:49 +0900

    Merge remote-tracking branch 'origin/test' into fukuda_login

--- KENInteriorShop/src/main/java/jp/ken/interiorshop/application/controlleradvice/GlobalControllerAdvice.java
+++ KENInteriorShop/src/main/java/jp/ken/interiorshop/application/controlleradvice/GlobalControllerAdvice.java
@@ -2,13 +2,11 @@ package jp.ken.interiorshop.application.controlleradvice;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jakarta.servlet.http.HttpSession;
 import jp.ken.interiorshop.presentation.form.MemberLoginForm;
 
 @ControllerAdvice
-@SessionAttributes("userLoggedIn")
 public class GlobalControllerAdvice {
 
 	 // ログインしているユーザー名
@@ -20,7 +18,7 @@ public class GlobalControllerAdvice {
     // ログインしているかどうか (true/false)
    @ModelAttribute("userLoggedIn")
    public boolean addUserLoggedIn(@ModelAttribute("loginUser")MemberLoginForm form) {
-	    if(form.getMail() != null && form.getPassword() != null) {
+	    if(form.getMail() != null) {
 	    	return true;
 	    } else {
 	    	return false;
